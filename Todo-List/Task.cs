8e06f92df196f3ede7c44e4d8ffb6173c20603ec
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-05-11 14:58:26 +0330

    [implement] UserTask class's body

--- Todo-List/Task.cs
+++ Todo-List/Task.cs
@@ -1,6 +1,25 @@
 ﻿namespace TodoApp.Task;
 
-class UserTask
+interface IUserTask
 {
+    public string Title { get; }
+    public string Description { get; }
+    public DateTime Date { get; }
+    public bool IsDone { get; }
+}
 
+class UserTask : IUserTask
+{
+    public UserTask(string title, string description, DateTime date, bool isDone)
+    {
+        Title = title;
+        Description = description;
+        Date = date;
+        IsDone = isDone;
+    }
+
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public DateTime Date { get; private set; }
+    public bool IsDone { get; private set; }
 }
